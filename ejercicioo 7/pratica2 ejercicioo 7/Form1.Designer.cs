ba306ca529d28afdac8dcab68cbe952f479cdedc
jordany22 <jimekaneki@gmail.com>
2025-06-25 00:08:17 -0400

    ejercicio 7

---
+++ ejercicioo 7/pratica2 ejercicioo 7/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace pratica2_ejercicioo_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInicio = new System.Windows.Forms.TextBox();
+            this.txtFin = new System.Windows.Forms.TextBox();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.lstPrimos = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtInicio
+            // 
+            this.txtInicio.Location = new System.Drawing.Point(239, 58);
+            this.txtInicio.Name = "txtInicio";
+            this.txtInicio.Size = new System.Drawing.Size(100, 20);
+            this.txtInicio.TabIndex = 0;
+            // 
+            // txtFin
+            // 
+            this.txtFin.Location = new System.Drawing.Point(468, 58);
+            this.txtFin.Name = "txtFin";
+            this.txtFin.Size = new System.Drawing.Size(100, 20);
+            this.txtFin.TabIndex = 1;
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(534, 271);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(75, 23);
+            this.btnMostrar.TabIndex = 3;
+            this.btnMostrar.Text = "Motrar numero  primos";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // lstPrimos
+            // 
+            this.lstPrimos.FormattingEnabled = true;
+            this.lstPrimos.Location = new System.Drawing.Point(239, 199);
+            this.lstPrimos.Name = "lstPrimos";
+            this.lstPrimos.Size = new System.Drawing.Size(120, 108);
+            this.lstPrimos.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstPrimos);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.txtFin);
+            this.Controls.Add(this.txtInicio);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInicio;
+        private System.Windows.Forms.TextBox txtFin;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.ListBox lstPrimos;
+    }
+}
+
---
+++ ejercicioo 7/pratica2 ejercicioo 7/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pratica2_ejercicioo_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            lstPrimos.Items.Clear();
+
+            if (int.TryParse(txtInicio.Text, out int inicio) && int.TryParse(txtFin.Text, out int fin))
+            {
+                for (int num = inicio; num <= fin; num++)
+                {
+                    if (EsPrimo(num))
+                    {
+                        lstPrimos.Items.Add(num);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ingrese valores numéricos válidos.");
+            }
+        }
+
+        private bool EsPrimo(int num)
+        {
+            if (num <= 1) return false;
+
+            for (int i = 2; i <= Math.Sqrt(num); i++)
+            {
+                if (num % i == 0) return false;
+            }
+            return true;
+        }
+    }
+    }
+
